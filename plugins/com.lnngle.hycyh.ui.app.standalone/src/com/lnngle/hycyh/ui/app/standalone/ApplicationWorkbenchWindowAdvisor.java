855db83e62fb8c8d8dd7a302ae2394e3353371b7
lnngle <lnngle@163.com>
2022-09-15 09:36:15 +0800

    dev

--- plugins/com.lnngle.hycyh.ui.app.standalone/src/com/lnngle/hycyh/ui/app/standalone/ApplicationWorkbenchWindowAdvisor.java
+++ plugins/com.lnngle.hycyh.ui.app.standalone/src/com/lnngle/hycyh/ui/app/standalone/ApplicationWorkbenchWindowAdvisor.java
@@ -6,8 +6,6 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.application.ActionBarAdvisor;
-import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.osgi.framework.Bundle;
@@ -16,10 +14,6 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
 	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
 		super(configurer);
-	}
-
-	public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-		return new ApplicationActionBarAdvisor(configurer);
 	}
 
 	public void preWindowOpen() {
