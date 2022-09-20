bf2f44af7bd2e29f5776202c26d8ced980f3be0d
lnngle <lnngle@163.com>
2022-09-20 16:09:54 +0800

    dev

---
+++ tests/com.lnngle.hycyh.ui.app.standalone.test/src/com/lnngle/hycyh/ui/app/standalone/test/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.lnngle.hycyh.ui.app.standalone.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+	@Test
+    public void test() {
+        assertTrue(true);
+    }
+}
