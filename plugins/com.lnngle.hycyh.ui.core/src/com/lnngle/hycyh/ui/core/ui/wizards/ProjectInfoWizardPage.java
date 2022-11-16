a0e02f559549458f5020ff4b7694c571207e406b
lnngle <lnngle@163.com>
2022-11-16 14:56:10 +0800

    dev

--- plugins/com.lnngle.hycyh.ui.core/src/com/lnngle/hycyh/ui/core/ui/wizards/ProjectInfoWizardPage.java
+++ plugins/com.lnngle.hycyh.ui.core/src/com/lnngle/hycyh/ui/core/ui/wizards/ProjectInfoWizardPage.java
@@ -1,12 +1,21 @@
 package com.lnngle.hycyh.ui.core.ui.wizards;
 
-import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 
-public class ProjectInfoWizardPage extends WizardNewProjectCreationPage {
+public class ProjectInfoWizardPage extends WizardPage {
 	public static final String PROJECT_INFO_WIZARD_PAGE_NAME = "ProjectInfoWizardPage";
 
 	public ProjectInfoWizardPage(String pageName) {
 		super(pageName);
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		
+		setControl(composite);
 	}
 
 }
